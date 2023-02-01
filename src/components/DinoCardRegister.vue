4930629bfd1741ed405bbf34a869b0bceca7d015
bevanshaw <bevan.shaw@capgemini.com>
2023-02-01 15:53:08 +1300

    Scaffolding

---
+++ src/components/DinoCardRegister.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const validCSC = ref(false);
+const firstName = ref("");
+const lastName = ref("");
+const nameRules = ref([
+  (v: any) => !!v || "Name is required",
+  (v: string | any[]) =>
+    v.length <= 10 || "Name must be less than 10 characters",
+]);
+const email = ref("");
+const emailRules = ref([
+  (v: any) => !!v || "E-mail is required",
+  (v: string) => /.+@.+/.test(v) || "E-mail must be valid",
+]);
+</script>
+
+<template>
+  <v-card class="bg-secondary">
+    <v-form v-model="validCSC">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="firstName"
+              :rules="nameRules"
+              :counter="10"
+              label="First name"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="lastName"
+              :rules="nameRules"
+              :counter="10"
+              label="Last name"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-card>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
-import IntroInformation from "../components/IntroInformation.vue";
+import InformationCard from "@/components/InformationCard.vue";
 import CustomToggleCheck from "@/components/CustomToggleCheck.vue";
-import CSCardForm from "@/components/CSCardForm.vue";
+import CommunityServicesCardForm from "@/components/CommunityServicesCardForm.vue";
+import DinoCardForm from "@/components/DinoCardForm.vue";
+import DinoCardRegisterForm from "@/components/DinoCardRegister.vue";
 </script>
 
 <template>
-  <IntroInformation
+  <InformationCard
     heading="Welcome to Dino Card"
     description="Dino Card is a government funded concession that gives community service cardholders with a registered Dino Card access to half-price public transport."
   />
   <CustomToggleCheck heading="Do you have a Community Services Card?" />
-  <CSCardForm />
+  <CommunityServicesCardForm />
+  <InformationCard
+    heading="You are not fossilized enough for a Dino Card!"
+    description="Dino Card is only available to community service cardholders allowing access to half-price public transport."
+  />
   <CustomToggleCheck heading="Do you have an existing Dino Card?" />
+  <DinoCardForm />
+  <DinoCardRegisterForm />
 </template>
 
 <style lang="scss" scoped></style>
